4f91e1c959244b5bd87015a801bb9cf8178a71de
messie Kimbembe <messiekimbembe01@gmail.com>
2026-01-03 16:51:19 +0100

    fix: Prevent readonly variable error when logger.sh is sourced multiple times
    - Add check before declaring readonly variables
    - Fixes error when script is sourced multiple times

--- druid-ingestion/lib/logger.sh
+++ druid-ingestion/lib/logger.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readonly RED='\033[0;31m' GREEN='\033[0;32m' YELLOW='\033[1;33m' NC='\033[0m'
+[ -z "${RED:-}" ] && readonly RED='\033[0;31m' GREEN='\033[0;32m' YELLOW='\033[1;33m' NC='\033[0m'
 readonly VERBOSE="${VERBOSE:-0}"
 
 log_info() { [[ "$VERBOSE" -ge 1 ]] && echo -e "${GREEN}[INFO]${NC} $*" >&2 || true; }
