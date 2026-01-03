297eaca4e4fc5d88d6070aef8eeb362f674c62ee
messie Kimbembe <messiekimbembe01@gmail.com>
2026-01-03 16:51:40 +0100

    fix: Prevent VERBOSE readonly error when logger.sh is sourced multiple times

--- druid-ingestion/lib/logger.sh
+++ druid-ingestion/lib/logger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ -z "${RED:-}" ] && readonly RED='\033[0;31m' GREEN='\033[0;32m' YELLOW='\033[1;33m' NC='\033[0m'
-readonly VERBOSE="${VERBOSE:-0}"
+[ -z "${VERBOSE_READONLY:-}" ] && readonly VERBOSE="${VERBOSE:-0}" && export VERBOSE_READONLY=1
 
 log_info() { [[ "$VERBOSE" -ge 1 ]] && echo -e "${GREEN}[INFO]${NC} $*" >&2 || true; }
 log_warn() { echo -e "${YELLOW}[WARN]${NC} $*" >&2; }
